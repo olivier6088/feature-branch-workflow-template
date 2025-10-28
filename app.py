ca30241d681f7affa0e813250487b0ed47a8ddea
Nairod97425 <dorian.hoa97425@gmail.com>
2025-10-28 13:04:01 +0400

    fix: Correction du menu burger (ajout du script Bootstrap JS)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Le Diabète à La Réunion</h1>"
+    return render_template('home.html')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
